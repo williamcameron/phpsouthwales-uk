2925f18023fbbc32dbebde7188b357da89926f1f
Oliver Davies <oliver@oliverdavies.uk>
2019-06-19 18:35:39 +0100

    Rename commands

--- helper.sh
+++ helper.sh
@@ -6,13 +6,13 @@ ARGS=$*
 
 case $OPERATION in
   'config-export') ddev exec drupal config:export $ARGS;;
-  'install')
+  'drupal-install')
     ddev exec drush site:install -y
     ddev exec drush config:set -y system.site uuid 002d5245-82cf-4586-b57f-5ab64ebcd877
     ddev exec drush config:delete -y shortcut.set.default
     ddev exec drush config:import -y --source=../config/sync
     ;;
-  'refresh')
+  'drupal-refresh')
     ddev composer install
     ddev exec drupal cache:rebuild
     ddev exec drupal config:import
