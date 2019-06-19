442cd6a930e81ad1d7680d4266415193809ed209
Oliver Davies <oliver@oliverdavies.uk>
2019-06-19 19:06:07 +0100

    Add and use config_install

--- helper.sh
+++ helper.sh
@@ -7,10 +7,9 @@ ARGS=$*
 case $OPERATION in
   'config-export') ddev exec drupal config:export $ARGS;;
   'drupal-install')
-    ddev exec drush site:install -y
-    ddev exec drush config:set -y system.site uuid 002d5245-82cf-4586-b57f-5ab64ebcd877
-    ddev exec drush config:delete -y shortcut.set.default
-    ddev exec drush config:import -y --source=../config/sync
+    ddev composer install
+    ddev exec drush si config_installer -y --account-name=admin123 --account-pass=admin123
+    ddev exec drupal cache:rebuild
     ;;
   'drupal-refresh')
     ddev composer install
