d1b0aa63112c8ea17f9bae4070012fd0effb950b
Oliver Davies <oliver@oliverdavies.uk>
2019-06-19 21:08:03 +0100

    Add blank lines for readability

--- helper.sh
+++ helper.sh
@@ -6,11 +6,13 @@ ARGS=$*
 
 case $OPERATION in
   'config-export') ddev exec drupal config:export $ARGS;;
+
   'drupal-install')
     ddev composer install
     ddev exec drush si config_installer -y --account-name=admin123 --account-pass=admin123
     ddev exec drupal cache:rebuild
     ;;
+
   'drupal-refresh')
     ddev composer install
     ddev exec drupal cache:rebuild
