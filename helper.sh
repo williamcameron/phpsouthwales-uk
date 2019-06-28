6a26ad2bc52819b4e2db07e3ae91690262e41c6b
Oliver Davies <oliver@oliverdavies.uk>
2019-06-28 21:51:29 +0100

    Build the theme assets during install and refresh

--- helper.sh
+++ helper.sh
@@ -4,12 +4,15 @@ OPERATION=$1
 shift
 ARGS=$*
 
+THEME_DIR="/var/www/html/web/themes/custom/phpsouthwales"
+
 case $OPERATION in
   'config-export') ddev exec drupal config:export $ARGS;;
 
   'drupal-install')
     ddev composer install
     ddev exec drush si config_installer -y --account-name=admin --account-pass=admin123
+    ./helper.sh build-theme
     ddev exec drupal cache:rebuild
     ;;
 
@@ -18,7 +21,13 @@ case $OPERATION in
     ddev exec drupal cache:rebuild
     ddev exec drupal config:import
     ddev exec drupal update:execute
+    ./helper.sh build-theme
     ddev exec drupal cache:rebuild
+    ;;
+
+  'build-theme')
+    ddev exec --dir $THEME_DIR yarn
+    ddev exec --dir $THEME_DIR yarn build
     ;;
 
   # Defer anything else to ddev.
