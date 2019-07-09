8974b0464e1beb4118e9c949ca3fa816a1a6b1c5
Oliver Davies <oliver@oliverdavies.uk>
2019-07-09 21:17:25 +0100

    Only load the queue once

--- web/modules/custom/event_pull/src/Service/Importer/EventImporter.php
+++ web/modules/custom/event_pull/src/Service/Importer/EventImporter.php
@@ -35,10 +35,10 @@ class EventImporter {
    * Import events.
    */
   public function import(): void {
+    $queue = Queue::load('event_pull');
     $this->eventLoader->getUpcoming()
-      ->each(function (Event $event): void {
+      ->each(function (Event $event) use ($queue): void {
         $job = Job::create('event_pull_pulled_event', $event->toArray());
-        $queue = Queue::load('default');
         $queue->enqueueJob($job);
       });
   }
